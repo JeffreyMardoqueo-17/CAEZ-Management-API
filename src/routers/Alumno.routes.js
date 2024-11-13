91d2f529ed663e8a8f0fe83e8e824c990f0db10f
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-11-12 19:12:32 -0600

    ✔🎇 crud de Alumno listo

--- src/routers/Alumno.routes.js
+++ src/routers/Alumno.routes.js
@@ -7,7 +7,7 @@ const route = Router();
 // Obtener todos los alumnos
 route.get('/alumnos', AlumnoController.getAllAlumnos);
 route.get('/alumnos/:id', validateToken, AlumnoController.getAlumnoById);
-// route.get('/alumnos/numDocumento/:numDocumento', validateToken, AlumnoController.ge);
+route.get('/alumnos/numDocumento/:numDocumento', validateToken, AlumnoController.getAlumnoByNumDocumento);
 route.post('/alumnos', validateToken, AlumnoController.createAlumno);
 route.put('/alumnos/:id', validateToken, AlumnoController.updateAlumno);
 route.delete('/alumnos/:id', validateToken, AlumnoController.deleteAlumno);
